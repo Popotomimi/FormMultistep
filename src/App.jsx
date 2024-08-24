8ef7fc711493ede6db2edc2dced79ea3af543224
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-08-23 22:10:35 -0300

    Second commit
    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+// Components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
 import "./App.css";
 
 function App() {
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+
   return (
-    <>
-      <h1>React Multistep</h1>
-    </>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto!
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
